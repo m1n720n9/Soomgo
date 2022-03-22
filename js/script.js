b1c17edbf21e37522178c31f8750bcbea60f2cde
m1n720n9 <skmnjng@gmail.com>
2022-03-22 18:08:16 +0900

    2203221808

--- js/script.js
+++ js/script.js
@@ -1,3 +1,57 @@
+const search = document.querySelector('.h_search input');
+const searchBorder = document.querySelector('.h_search');
+const searchWrap = document.querySelector('.search_tab_wrap');
+const tabItem = document.querySelectorAll('.tab_keyword .item');
+const tabTitle = document.querySelector('.tab_title ul>li>a');
+
+
+tabTitle.addEventListener("click", function(){
+  tabTitle.classList.add('active');
+
+});
+
+
+function clickSearch (){
+  searchWrap.classList.add('active');
+  searchBorder.classList.add('active');
+};
+
+search.addEventListener("click", clickSearch);
+
+for(let i=0; i<tabItem.length; i++){
+  tabItem[i].addEventListener("mouseover", function(){
+    tabItem[i].classList.add('active');
+  });
+  tabItem[i].addEventListener("mouseout", function(){
+    tabItem[i].classList.remove('active');
+  });
+};
+
+
+
+ 
+
+/* tabItem.addEventListener("mouseover", function(){
+  tabItem.classList.add('active');
+})
+ */
+
+
+/* search.addEventListener("click", function (){
+  searchWrap.classList.add('active')
+});
+ */
+
+
+
+
+
+
+
+
+
+
+/* slider */
 var bannerS = new Swiper(".bannerS", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -56,30 +110,25 @@ var b_pro_S = new Swiper(".b_pro_S", {
 });
 
 var story_S = new Swiper(".story_S", {
-  slidesPerView: 3,
-  spaceBetween: 16,
+  slidesPerView: 2,
+  spaceBetween: 8,
   slidesPerGroup: 1,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   },
-  /* breakpoints: {
-    481: {
-      slidesPerView: 4,  //브라우저가 481보다 클 때
-      spaceBetween: 12,
-      slidesPerGroup: 1,
-    },
+  breakpoints: {
     769: {
-      slidesPerView: 5,  //브라우저가 481보다 클 때
-      spaceBetween: 12,
+      slidesPerView: 3,  //브라우저가 991보다 클 때
+      spaceBetween: 16,
       slidesPerGroup: 1,
     },
     991: {
-      slidesPerView: 6,  //브라우저가 991보다 클 때
+      slidesPerView: 3,  //브라우저가 991보다 클 때
       spaceBetween: 16,
-      slidesPerGroup: 2,
+      slidesPerGroup: 1,
     },
-  }, */
+  },
 });
 
 var ps_S = new Swiper(".ps_S", {
